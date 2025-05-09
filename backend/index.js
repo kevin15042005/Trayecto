e188188331e64c38a1939186394895ad8c5c5528
Kevin Alejandro Reyes Sarmeinto <kevin2013118@gmail.com>
2025-05-09 10:48:00 -0500

    Se agrega en el index,el get

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,29 @@
 import express from "express";
-import cors from "cors"
-import db from "./db.js"
-import administrador from "./routes/Administrador.js"
-import IngresoTrayecto from "./routes/trayectos.js"
+import cors from "cors";
+import db from "./db.js";
+import administrador from "./routes/Administrador.js";
+import IngresoTrayecto from "./routes/trayectos.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/administrador", administrador);   
-app.use("/api/trayecto", IngresoTrayecto)   
+app.use("/api/trayecto", IngresoTrayecto);   
+
+// Ruta raíz para evitar error 404 en Render
+app.get("/", (req, res) => {
+    res.send("API funcionando correctamente.");
+});
 
 app.listen(8080, () => {
     console.log("Servidor corriendo en http://trayecto.onrender.com");
 });
+
 process.on("SIGINT", () => {
     db.end((err) => {
         if (err) console.log("Error cerrando a MYSQL", err);
         else console.log("Conexion a MYSQL cerrada");
-        process.exit()
-    })
-})
+        process.exit();
+    });
+});
