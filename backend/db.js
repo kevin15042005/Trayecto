837ba94c2663ecef4a91e2686320ea933b8a8b3a
Kevin Alejandro Reyes Sarmeinto <kevin2013118@gmail.com>
2025-05-09 09:52:33 -0500

    se agrega para que inico automaticamente con render y con raiylwil inicia los dos y se cambia la base de datos

--- backend/db.js
+++ backend/db.js
@@ -2,19 +2,21 @@ import mysql from "mysql2";
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
 });
+
+
+
 
 db.connect((err) => {
   if (err) {
-    console.error("Error al conectar a la base de datos", err);
-    return;
-  }
-  console.log("Conectado a la base de datos MySQL");
+      console.error("Error al concetar en la base de datos", err)
+          return;
+    }
+    console.log("conectado a la base de datos MYSQL")
 });
 
 export default db;
-
